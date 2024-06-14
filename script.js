0805512d79466b373969a05238a61a5d148801d0
Heitor.Dimartini <108880461+heitor2203@users.noreply.github.com>
2024-06-14 11:36:07 -0300

    xx

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ const caixaPergunta = document.querySelector(".caixa-perguntas");
 const caixaalternativa = document.querySelector(".caixa-alternativa");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+const = pergunta [
+    {
+        enunciado: "pergunta 1",
+        alternativa:[
+            "alternativa 1",
+            "alternativa 2"
+]
+},
+{
+    enunciado: "pergunta 2"
+    alternativa: [
+        "alternativa 1",
+        "alternativa 2"
+    ]
+}
+];
